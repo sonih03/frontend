0406974d5832b52c3b1796f2394f1b41e7bbd2af
sonih03 <sonih03@naver.com>
2026-05-21 21:28:02 +0900

    employee, register, update, delete

--- src/no2_components/employee/EmployeeRegister.jsx
+++ src/no2_components/employee/EmployeeRegister.jsx
@@ -1,7 +1,32 @@
 import React, { useState } from 'react'
+import { useActionData } from 'react-router-dom'
 
 const initialEmp = {
   id: '', name: '', email: '', job: '', pay:''
+}
+
+const initialEmps = [
+    {id: "1", name: "John", email: "john@example.com", job: "frontend", pay: 600},
+    {id: "2", name: "Peter", email: "peter@example.com", job: "backend", pay: 600},
+    {id: "3", name: "Susan", email: "susan@example.com", job: "db", pay: 600},
+    {id: "4", name: "Sue", email: "sue@example.com", job: "ai", pay: 600},
+]
+
+const reducer = (state,action) =>{
+  switch(action.type){
+    case "change" : 
+      const{name,value} = event.target;
+      return
+        {
+          ...state,
+          emp: {...state.emp,[name] : value}
+        }
+  }
+}
+
+const initialstate = {
+  empTable: initialEmps,
+  emp: initialEmp
 }
 
 const EmployeeRegister = ({setState}) => {
